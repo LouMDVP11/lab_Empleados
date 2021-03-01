6a8399de695dd67b622548fc148dd3e2986efcc8
LouMDVP11 <66640684+LouMDVP11@users.noreply.github.com>
2021-03-01 10:18:37 -0600

    Terminado el diseño de la interfaz

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnCargarDatos = new System.Windows.Forms.Button();
+            this.dtgEmpleados = new System.Windows.Forms.DataGridView();
+            this.dtgAsistencia = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgEmpleados)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgAsistencia)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCargarDatos
+            // 
+            this.btnCargarDatos.Location = new System.Drawing.Point(210, 353);
+            this.btnCargarDatos.Name = "btnCargarDatos";
+            this.btnCargarDatos.Size = new System.Drawing.Size(111, 34);
+            this.btnCargarDatos.TabIndex = 0;
+            this.btnCargarDatos.Text = "Cargar Datos";
+            this.btnCargarDatos.UseVisualStyleBackColor = true;
+            // 
+            // dtgEmpleados
+            // 
+            this.dtgEmpleados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgEmpleados.Location = new System.Drawing.Point(27, 21);
+            this.dtgEmpleados.Name = "dtgEmpleados";
+            this.dtgEmpleados.RowHeadersWidth = 51;
+            this.dtgEmpleados.RowTemplate.Height = 24;
+            this.dtgEmpleados.Size = new System.Drawing.Size(524, 94);
+            this.dtgEmpleados.TabIndex = 1;
+            // 
+            // dtgAsistencia
+            // 
+            this.dtgAsistencia.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgAsistencia.Location = new System.Drawing.Point(27, 141);
+            this.dtgAsistencia.Name = "dtgAsistencia";
+            this.dtgAsistencia.RowHeadersWidth = 51;
+            this.dtgAsistencia.RowTemplate.Height = 24;
+            this.dtgAsistencia.Size = new System.Drawing.Size(524, 177);
+            this.dtgAsistencia.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(573, 91);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(363, 24);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(667, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Sueldo empleado";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(968, 595);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dtgAsistencia);
+            this.Controls.Add(this.dtgEmpleados);
+            this.Controls.Add(this.btnCargarDatos);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgEmpleados)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgAsistencia)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCargarDatos;
+        private System.Windows.Forms.DataGridView dtgEmpleados;
+        private System.Windows.Forms.DataGridView dtgAsistencia;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
