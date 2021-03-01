38876b659f3c75e0f6c5564fa799d29d778d6155
LouMDVP11 <66640684+LouMDVP11@users.noreply.github.com>
2021-03-01 10:32:39 -0600

    Encapsulamiento de variables en las clases empleado y asistencia

---
+++ clsAsistencia.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_Empleados
+{
+    class clsAsistencia
+    {
+        int codigo;
+        int horasMes;
+        string mes;
+
+        public int Codigo { get => codigo; set => codigo = value; }
+        public int HorasMes { get => horasMes; set => horasMes = value; }
+        public string Mes { get => mes; set => mes = value; }
+    }
+}
---
+++ clsEmpleado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_Empleados
+{
+    class clsEmpleado
+    {
+        int codigo;
+        string nombre;
+        double sueldo;
+
+        public int Codigo { get => codigo; set => codigo = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public double Sueldo { get => sueldo; set => sueldo = value; }
+    }
+}
